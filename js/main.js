40efad3236446bc2293c8124e5e9fc768f48a475
Hudson Kennedy <hudson.junior@castgroup.com.br>
2023-09-24 11:36:24 -0300

    Iniciando refatoração de portfólio em dois idiomas, usando somente um HTML

--- js/main.js
+++ js/main.js
@@ -296,3 +296,103 @@ function bodyScrollingToggle() {
     }
   });
 })();
+
+/*------------------Change language function----------------------- */
+
+// Função para trocar para inglês
+function loadEnglishContent() {
+  document.getElementById('menu-inicio').textContent = 'Home';
+  document.getElementById('menu-sobre').textContent = 'About';
+  document.getElementById('menu-portfolio').textContent = 'Portfolio';
+  document.getElementById('menu-contato').textContent = 'Contact';
+  document.getElementById('titulo-principal').textContent = 'Hudson Kennedy';
+    document.getElementById('texto-principal').textContent = 'Fullstack Developer / Software Engineer.';
+    document.getElementById('saiba-mais').textContent = 'Learn more';
+    document.getElementById('titulo-sobre-mim').textContent = 'About Me';
+    document.getElementById('texto-sobre-mim').innerHTML = `
+        In contact with the technology field for over 10 years, where I started developing in VB.net,
+        during the learning journey, I took some courses. In 2018, I started my degree in Business
+        Process Management and graduated in 2020. During this period, I continued to be in touch with
+        the technology field, where I began studying JavaScript and TypeScript, to work specifically on
+        the Frontend, for which I also took various courses to improve myself and evolve professionally.</br>
+        In 2021, I started my MBA in Software Engineering, graduated in 2022 where I learned many things
+        about Backend, and so on...</br>
+        In February 2023, I started my Master's degree as part of the Computer Science graduate program
+        at Harvard University and graduated.</br>
+        I will continue to improve myself as a Fullstack Developer and Software Engineer, gaining more
+        experience and contributing to my team!
+    `;
+    document.getElementById('openModalBtn').textContent = 'Resume';
+    document.getElementById('titulo-modal').textContent = 'Choose the CV language';
+    document.getElementById('inglesBtn').textContent = 'English';
+    document.getElementById('portuguesBtn').textContent = 'Portuguese';
+}
+
+// Função para trocar para português
+function loadPortugueseContent() {
+  document.getElementById('menu-inicio').textContent = 'Início';
+  document.getElementById('menu-sobre').textContent = 'Sobre';
+  document.getElementById('menu-portfolio').textContent = 'Portfólio';
+  document.getElementById('menu-contato').textContent = 'Contato';
+  document.getElementById('titulo-principal').textContent = 'Hudson Kennedy';
+    document.getElementById('texto-principal').textContent = 'Desenvolvedor Fullstack / Engenheiro de Software.';
+    document.getElementById('saiba-mais').textContent = 'Saiba mais';
+    document.getElementById('titulo-sobre-mim').textContent = 'Sobre mim';
+    document.getElementById('texto-sobre-mim').innerHTML = `
+        Em contato com a área de tecnologia por mais de 10 anos, onde comecei a desenvolver em VB.net,
+        durante a jornada de aprendizado, fui fazendo alguns cursos, em 2018 iniciei meu Tecnólogo em
+        processos gerenciais e me formei em 2020, neste período continuei em contato com a área de tecnologia,
+        onde comecei a estudar Javascript e Typescript, para trabalhar especificamente no Frontend, que também fiz
+        diversos cursos para me aprimorar, e evoluir profissionalmente.</br>
+        Em 2021 iniciei o MBA em Engenharia de Software, me formei em 2022 onde aprendi muitas coisas sobre
+        Backend e etc...</br>
+        Em fevereiro de 2023 iniciei meu Mestrado que atua dentro do programa de pós-graduação em Ciência da
+        Computação, na Universidade de Harvard e me formei.</br>
+        Continuarei me aprimorando como Desenvolvedor Fullstack e Engenheiro de Software, adquirindo mais
+        experiência e contribuindo com meu time!
+    `;
+    document.getElementById('openModalBtn').textContent = 'Currículo';
+    document.getElementById('titulo-modal').textContent = 'Escolha o idioma do currículo';
+    document.getElementById('inglesBtn').textContent = 'Inglês';
+    document.getElementById('portuguesBtn').textContent = 'Português';
+
+}
+
+
+// Detectar o idioma do navegador
+function detectLanguageAndLoadContent() {
+const languageNavigator = navigator.language || navigator.userLanguage;
+
+if (languageNavigator.startsWith('en')) {
+    loadEnglishContent();
+} else {
+    loadPortugueseContent();
+ }
+}
+detectLanguageAndLoadContent();
+
+const openModalBtn = document.getElementById('openModalBtn');
+const modal = document.getElementById('modal');
+const portuguesBtn = document.getElementById('portuguesBtn');
+const inglesBtn = document.getElementById('inglesBtn');
+
+
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+portuguesBtn.addEventListener('click', () => {
+  window.open('./assets/cv/HudsonKennedy-BR.pdf', '_blank');
+  modal.style.display = 'none';
+});
+
+inglesBtn.addEventListener('click', () => {
+  window.open('./assets/cv/HudsonKennedy-US.pdf', '_blank');
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
