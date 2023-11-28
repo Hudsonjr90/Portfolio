a2e747429a2651db868b1ecf970fb7dfb61d3c97
Hudson Kennedy <hudsonhugo@bol.com.br>
2023-11-28 18:37:04 -0300

    implementando scroller horizontal no rodapé

--- js/main.js
+++ js/main.js
@@ -732,3 +732,19 @@ document
   .getElementById("whatsapp-link")
   .addEventListener("click", openWhatsApp);
 document.getElementById("email-link").addEventListener("click", openEmail);
+
+const scrollers = document.querySelectorAll(".scroller");
+      
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+
+    Array.from(scrollerInner.children).forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
