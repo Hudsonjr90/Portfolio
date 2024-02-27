126fca4cb2185aafb3045aafd6fa66eab38e48a7
Hudson Kennedy <hudsonhugo@bol.com.br>
2024-02-27 17:55:57 -0300

    Feat: implementação do slide lateral dos idiomas em navbar, e aplicação de useEffect para trocar bandeira informativa do idioma selecionado.

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,105 +1,121 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { GrLanguage } from "react-icons/gr";
 import { Us, Fr, Br, Es, It } from "react-flags-select";
 import styles from "./LanguageSwitcher.module.css";
+import { motion } from "framer-motion";
 
 const LanguageSwitcher = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
   const [soundClick, setSoundClick] = useState<boolean>(false);
+  const [currentLanguage, setCurrentLanguage] = useState<string>("pt");
+  const [SidebarOpen, setSidebarOpen] = useState(false);
 
-  const changeLanguage = (lng: string | undefined) => {
+  const changeLanguage = (lng: string ) => {
     i18n.changeLanguage(lng);
-    switch (lng) {
-      case "en":
-        break;
-      case "pt":
-        break;
-      case "fr":
-        break;
-      case "it":
-        break;
-      case "es":
-        break;
-      default:
-        break;
-    }
-    setDropdownOpen(false);
+    setCurrentLanguage(lng || "");
   };
+  
 
-  const handleDropdownToggle = () => {
-    setDropdownOpen(!isDropdownOpen);
+  const toggleSidebar = () => {
+    setSidebarOpen(!SidebarOpen);
   };
 
   const handleAudioButtonClick = () => {
     const audio = new Audio("/sounds/button_click.mp3");
-
+  
     if (soundClick) {
-      audio.pause();
+     audio.pause();    
     } else {
       audio.play();
     }
   };
 
+  useEffect(() => {
+    setSidebarOpen(false);
+  }, [currentLanguage]);
+
+
   return (
     <div className={styles.lng_box}>
-      <div className={styles.dropdown}>
-        <button className={styles.lng_btn}>
-          <GrLanguage
-            onClick={() => {
-              handleDropdownToggle();
-              handleAudioButtonClick();
-            }}
-          />
+      <div className={styles.slide}>
+      <button className={styles.lng_btn}
+       onClick={() => {
+        toggleSidebar();
+        handleAudioButtonClick(); 
+      }}>
+          {currentLanguage === "pt" && <Br className={styles.flags} />}
+          {currentLanguage === "en" && <Us className={styles.flags} />}
+          {currentLanguage === "fr" && <Fr className={styles.flags} />}
+          {currentLanguage === "it" && <It className={styles.flags} />}
+          {currentLanguage === "es" && <Es className={styles.flags} />}
         </button>
-        {isDropdownOpen && (
-          <div className={styles.dropdown_content}>
+        {SidebarOpen && (
+          <motion.div
+            className={styles.sidebar}
+            initial={{ x: "80%" }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", stiffness: 120 }}
+          >
             <button
               onClick={() => {
                 changeLanguage("pt");
-                handleAudioButtonClick();
+                handleAudioButtonClick(); 
               }}
             >
-              <Br className={styles.flags} />
+              <div className={styles.flags_name}>
+                <Br className={styles.flags} />
+                <span>{t("name.br")}</span>
+              </div>
             </button>
             <button
               onClick={() => {
                 changeLanguage("en");
-                handleAudioButtonClick();
+                handleAudioButtonClick(); 
               }}
             >
-              <Us className={styles.flags} />
+              <div className={styles.flags_name}>
+                <Us className={styles.flags} />
+                <span>{t("name.us")}</span>
+              </div>
             </button>
-            
+
             <button
               onClick={() => {
                 changeLanguage("fr");
-                handleAudioButtonClick();
+                handleAudioButtonClick(); 
               }}
             >
-             <Fr className={styles.flags} />
+              <div className={styles.flags_name}>
+                <Fr className={styles.flags} />
+                <span>{t("name.fr")}</span>
+              </div>
             </button>
 
             <button
               onClick={() => {
                 changeLanguage("it");
-                handleAudioButtonClick();
+                handleAudioButtonClick(); 
               }}
             >
-             <It className={styles.flags} />
+              <div className={styles.flags_name}>
+                <It className={styles.flags} />
+                <span>{t("name.it")}</span>
+              </div>
             </button>
 
             <button
               onClick={() => {
                 changeLanguage("es");
-                handleAudioButtonClick();
+                handleAudioButtonClick(); 
               }}
             >
-             <Es className={styles.flags} />
+              <div className={styles.flags_name}>
+                <Es className={styles.flags} />
+                <span>{t("name.es")}</span>
+              </div>
             </button>
-            
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
