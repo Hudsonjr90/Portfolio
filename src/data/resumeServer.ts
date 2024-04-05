be15ae4dd4b227bdfc3e0ef8ac4de850ad0b4831
Hudson Kennedy <hudsonhugo@bol.com.br>
2024-04-04 23:13:41 -0300

    fix: ajuste no modal do curriculo, para apresentar a quarta página.

--- src/data/resumeServer.ts
+++ src/data/resumeServer.ts
@@ -3,26 +3,31 @@ const resumeServer  = {
       "/imgs/imgCv/HudsonKennedyBR-1.png",
       "/imgs/imgCv/HudsonKennedyBR-2.png",
       "/imgs/imgCv/HudsonKennedyBR-3.png",
+      "/imgs/imgCv/HudsonKennedyBR-4.png",
     ],
     us: [
       "/imgs/imgCv/HudsonKennedyUS-1.png",
       "/imgs/imgCv/HudsonKennedyUS-2.png",
       "/imgs/imgCv/HudsonKennedyUS-3.png",
+      "/imgs/imgCv/HudsonKennedyUS-4.png",
     ],
     fr: [
       "/imgs/imgCv/HudsonKennedyFR-1.png",
       "/imgs/imgCv/HudsonKennedyFR-2.png",
       "/imgs/imgCv/HudsonKennedyFR-3.png",
+      "/imgs/imgCv/HudsonKennedyFR-4.png",
     ],
     it: [
       "/imgs/imgCv/HudsonKennedyIT-1.png",
       "/imgs/imgCv/HudsonKennedyIT-2.png",
       "/imgs/imgCv/HudsonKennedyIT-3.png",
+      "/imgs/imgCv/HudsonKennedyIT-4.png",
     ],
     es: [
       "/imgs/imgCv/HudsonKennedyES-1.png",
       "/imgs/imgCv/HudsonKennedyES-2.png",
       "/imgs/imgCv/HudsonKennedyES-3.png",
+      "/imgs/imgCv/HudsonKennedyES-4.png",
     ],
   };
 
