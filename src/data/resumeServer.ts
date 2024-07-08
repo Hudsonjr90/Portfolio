8641654125abd9c06029d0d068c52d529b0524ea
Hudson Kennedy <hudsonhugo@bol.com.br>
2024-07-08 19:49:48 -0300

    fix: ajuste de import

--- src/data/resumeServer.ts
+++ src/data/resumeServer.ts
@@ -2,37 +2,37 @@ const resumeServer = {
   
   br: [
     "../../src/assets/imgs/imgCv/HudsonKennedyBR-1.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyBR-2.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyBR-3.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyBR-4.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyBR-2.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyBR-3.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyBR-4.png",
   ],
 
   us: [
-    "./../src/assets/imgs/imgCv/HudsonKennedyUS-1.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyUS-2.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyUS-3.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyUS-4.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyUS-1.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyUS-2.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyUS-3.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyUS-4.png",
   ],
 
   fr: [
-    "./../src/assets/imgs/imgCv/HudsonKennedyFR-1.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyFR-2.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyFR-3.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyFR-4.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyFR-1.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyFR-2.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyFR-3.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyFR-4.png",
   ],
 
   it: [
-    "./../src/assets/imgs/imgCv/HudsonKennedyIT-1.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyIT-2.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyIT-3.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyIT-4.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyIT-1.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyIT-2.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyIT-3.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyIT-4.png",
   ],
 
   es: [
-    "./../src/assets/imgs/imgCv/HudsonKennedyES-1.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyES-2.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyES-3.png",
-    "./../src/assets/imgs/imgCv/HudsonKennedyES-4.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyES-1.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyES-2.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyES-3.png",
+    "../../src/assets/imgs/imgCv/HudsonKennedyES-4.png",
   ],
 
 }
