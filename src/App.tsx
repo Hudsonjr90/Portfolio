43d6e2c15911951b9892447c97b077884124c90e
Hudson Kennedy <hudsonhugo@bol.com.br>
2024-07-11 10:10:07 -0300

    fix: ajustes gerais

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Skills from './pages/Skills/Skills'
 import Contact from './pages/Contact/Contact'
 import { ThemeProvider } from './context/ThemeContext'
 import { I18nextProvider } from 'react-i18next'
-import i18n from '../i18n'
+import i18n from './config/i18n'
 import { AnimatePresence } from 'framer-motion'
 
 function App() {
--- src/config/i18n.tsx
+++ src/config/i18n.tsx
@@ -1,10 +1,10 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import ptTranslation from "./src/locales/pt.json"
-import enTranslation from "./src/locales/en.json"
-import esTranslation from "./src/locales/es.json"
-import frTranslation from "./src/locales/fr.json"
-import itTranslation from "./src/locales/it.json"
+import ptTranslation from "./locales/pt.json"
+import enTranslation from "./locales/en.json"
+import esTranslation from "./locales/es.json"
+import frTranslation from "./locales/fr.json"
+import itTranslation from "./locales/it.json"
 
 
 i18n
