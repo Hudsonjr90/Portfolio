b244e1df7b74f32af2afac43727dff4081faebdf
Hudson Kennedy <hudsonhugo@bol.com.br>
2024-07-23 13:44:21 -0300

    Fix: ajustes desempenho

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ filename: 'stats.html', open: true })
+  ],
   css: {
     modules: {
       generateScopedName: '[name]__[local]___[hash:base64:5]',
