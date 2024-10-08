de08abbe6f6791ead4cad6f6a85cd615c75cbebb
Hudson Kennedy <hudsonhugo@bol.com.br>
2024-10-08 01:01:19 -0300

    fix: ajuste no card component para fazer download de diplomas

--- src/components/Card/CardComponent.tsx
+++ src/components/Card/CardComponent.tsx
@@ -1,100 +1,127 @@
-import Card from 'react-bootstrap/Card'
-import styles from './CardComponent.module.css'
-import Paginate from 'react-paginate'
-import { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
-import cardsServer from '../../data/cardsServer'
-import { useTranslation } from 'react-i18next'
+import Card from "react-bootstrap/Card";
+import styles from "./CardComponent.module.css";
+import Paginate from "react-paginate";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import cardsServer from "../../data/cardsServer";
+import { useTranslation } from "react-i18next";
+import { Button } from "react-bootstrap";
+import { FaDownload } from "react-icons/fa";
 
 const CardComponent = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
-  const [hoveredCard, setHoveredCard] = useState<number | null>(null)
+  const [hoveredCard, setHoveredCard] = useState<number | null>(null);
+  const [showDownloadButton, setShowDownloadButton] = useState(false);
 
   const handleMouseEnter = (id: number) => {
-    setHoveredCard(id)
-  }
+    setHoveredCard(id);
+    setShowDownloadButton(true);
+  };
 
   const handleMouseLeave = () => {
-    setHoveredCard(null)
-  }
+    setHoveredCard(null);
+    setShowDownloadButton(false);
+  };
 
-  const [currentPage, setCurrentPage] = useState(0)
-  const [cardsPerPage, setCardsPerPage] = useState(1)
+  const handleDownload = (cardId: number) => {
+    const card = cardsServer.find((c) => c.id === cardId);
+  
+    if (card && card.file && card.text) { 
+      const link = document.createElement("a");
+      link.href = card.file; 
+      link.setAttribute("download", `${card.text}.pdf`);
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+  
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [cardsPerPage, setCardsPerPage] = useState(1);
 
   useEffect(() => {
     function handleResize() {
       if (window.innerWidth < 769) {
-        setCardsPerPage(2)
+        setCardsPerPage(2);
       } else {
-        setCardsPerPage(1)
+        setCardsPerPage(1);
       }
     }
 
-    handleResize()
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-  const offset = currentPage * cardsPerPage
-  const pageCount = Math.ceil(cardsServer.length / cardsPerPage)
+  const offset = currentPage * cardsPerPage;
+  const pageCount = Math.ceil(cardsServer.length / cardsPerPage);
 
   const handlePageClick = ({
     selected: selectedPage,
   }: {
-    selected: number
+    selected: number;
   }) => {
-    setCurrentPage(selectedPage)
-  }
+    setCurrentPage(selectedPage);
+  };
 
   const autoChangePage = () => {
     if (!hoveredCard) {
-      const nextPage = (currentPage + 1) % pageCount
-      setCurrentPage(nextPage)
+      const nextPage = (currentPage + 1) % pageCount;
+      setCurrentPage(nextPage);
     }
-  }
+  };
 
   useEffect(() => {
-    const intervalId = setInterval(autoChangePage, 5000)
+    const intervalId = setInterval(autoChangePage, 5000);
 
-    return () => clearInterval(intervalId)
-  }, [currentPage, pageCount, hoveredCard])
-
- 
+    return () => clearInterval(intervalId);
+  }, [currentPage, pageCount, hoveredCard]);
 
   const currentPageData = cardsServer
     .slice(offset, offset + cardsPerPage)
     .map((card) => (
-     
-        <Card
-          className={styles.card_content}
-          key={card.id}
-          onMouseEnter={() => handleMouseEnter(card.id)}
-          onMouseLeave={handleMouseLeave}
-        >
-          <Card.Img
-            src={card.img}
-            alt="Card image"
-            className={hoveredCard === card.id ? styles.blur : ''}
-            loading="lazy"
-          />
-          <Card.ImgOverlay>
-            <Card.Text
-               className={`${styles.card_text} ${
-                hoveredCard === card.id ? styles.showText : ''
-               }`}
+      <Card
+        className={styles.card_content}
+        key={card.id}
+        onMouseEnter={() => handleMouseEnter(card.id)}
+        onMouseLeave={handleMouseLeave}
+      >
+        <Card.Img
+          src={card.img}
+          alt="Card image"
+          className={hoveredCard === card.id ? styles.blur : ""}
+          loading="lazy"
+        />
+        <Card.ImgOverlay>
+          <Card.Text
+            className={`${styles.card_text} ${hoveredCard === card.id ? styles.showText : ""}`}
+          >
+            {t(`education.cards.${card.id}.text`)}
+          </Card.Text>
+          {showDownloadButton && (
+            <Button
+              className={styles.download_button}
+              variant="outline-primary"
+              size="sm"
+              onClick={() => handleDownload(card.id)}
             >
-              {t(`education.cards.${card.id}.text`)}
-            </Card.Text>
-          </Card.ImgOverlay>
-        </Card>
-    ))
+             {t('home.download')}
+            <FaDownload className={styles.size_button} />
+            </Button>
+            
+          )}
+        </Card.ImgOverlay>
+      </Card>
+    ));
 
   return (
     <>
       <motion.div
-        initial={{ opacity: 0, x: '-80%' }}
-        animate={{ opacity: 1, x: '0%' }}
+        initial={{ opacity: 0, x: "-80%" }}
+        animate={{ opacity: 1, x: "0%" }}
         transition={{
           duration: 2,
           delay: 0.3,
@@ -105,8 +132,8 @@ const CardComponent = () => {
         {currentPageData}
       </motion.div>
       <motion.div
-        initial={{ opacity: 0, y: '80%' }}
-        animate={{ opacity: 1, y: '0%' }}
+        initial={{ opacity: 0, y: "80%" }}
+        animate={{ opacity: 1, y: "0%" }}
         transition={{
           duration: 2,
           delay: 0.3,
@@ -118,17 +145,17 @@ const CardComponent = () => {
           pageRangeDisplayed={5}
           marginPagesDisplayed={0}
           onPageChange={({ selected: selectedPage }) => {
-            handlePageClick({ selected: selectedPage })
+            handlePageClick({ selected: selectedPage });
           }}
           containerClassName={styles.pagination}
           activeClassName={styles.activePage}
-          previousLabel={'<<'}
-          nextLabel={'>>'}
+          previousLabel={"<<"}
+          nextLabel={">>"}
           forcePage={currentPage}
         />
       </motion.div>
     </>
-  )
-}
+  );
+};
 
-export default CardComponent
+export default CardComponent;
--- src/data/cardsServer.ts
+++ src/data/cardsServer.ts
@@ -1,120 +1,148 @@
 const cardsServer = [
-
   {
     id: 0,
     img: "/imgs/imgEducation/bootcamp.webp",
     text: "Digital Innovation - Bootcamp Banco Pan(Desenvolvimento Frontend com Angular)",
+    file: "/formations/bootcamp.pdf"
   },
   {
     id: 1,
     img: "/imgs/imgEducation/harvard.webp",
     text: "Universidade de Harvard - Mestrado que atua dentro do programa de pós-graduação em Ciência da Computação",
+    file: "/formations/harvard.pdf"
   },
   {
     id: 2,
     img: "/imgs/imgEducation/mba.webp",
     text: "Faculdade Única de MG - Pós Graduação em Engenharia de Software",
+    file: "/formations/mba.pdf"
   },
   {
     id: 3,
     img: "/imgs/imgEducation/estacio.webp",
     text: "Faculdade Estácio de Sá - Processos Gerenciais",
+    file: "/formations/processos-gerenciais.pdf"
   },
   {
     id: 4,
     img: "/imgs/imgEducation/dio-comp.webp",
     text: "Digital Innovation - Trabalhando com Componentes em React",
+    file: "/formations/componentes-react.pdf"
   },
   {
     id: 5,
     img: "/imgs/imgEducation/dio-arq.webp",
     text: "Digital Innovation - Arquitetura de Componentes e a gestão da complexidade no front-end",
+    file: "/formations/arquitetura-componentes.pdf"
   },
   {
     id: 6,
     img: "/imgs/imgEducation/dio-log.webp",
     text: "Digital Innovation - Lógica de Programção Essencial",
+    file: "/formations/logica-programacao.pdf"
   },
   {
     id: 7,
     img: "/imgs/imgEducation/dio-es6.webp",
     text: "Digital Innovation - Javascript ES6 Essencial",
+    file: "/formations/javascript-es6.pdf"
   },
   {
     id: 8,
     img: "/imgs/imgEducation/dio-ang8.webp",
     text: "Digital Innovation - Técnicas Avançadas em Angular 8",
+    file: "/formations/angular8.pdf"
   },
   {
     id: 9,
     img: "/imgs/imgEducation/aem-front.webp",
     text: "Adobe Profissional - AEM(Front-end Developer)",
+    file: "/formations/aem-frontend.pdf"
   },
   {
     id: 10,
     img: "/imgs/imgEducation/aem-sbp.webp",
     text: "Adobe Profissional - AEM(Sites Business Practitioner)",
+    file: "/formations/aem-sbp.pdf"
   },
   {
     id: 11,
     img: "/imgs/imgEducation/vuejs.webp",
     text: "VueJs Brasil - Curso Vuejs(Básico ao Avançado)",
+    file: "/formations/vuejs.pdf"
   },
   {
     id: 12,
     img: "/imgs/imgEducation/dio-vue1.webp",
-    text: "Digital Innovation - Diretivas e Propriedades de Componentes Vue.js ",
+    text: "Digital Innovation - Diretivas e Propriedades de Componentes Vue.js",
+    file: "/formations/diretivas-vue.pdf"
   },
   {
     id: 13,
     img: "/imgs/imgEducation/dio-vue2.webp",
     text: "Digital Innovation - Componentes, Métodos e Ciclo de vida com Vue.js",
+    file: "/formations/componentes-vue.pdf"
   },
   {
     id: 14,
     img: "/imgs/imgEducation/udemy.webp",
     text: "Udemy - Curso Javascript e Typescript do Básico ao Avançado",
+    file: "/formations/javascript-typescript.pdf"
   },
   {
     id: 15,
     img: "/imgs/imgEducation/sebrae-EF.webp",
     text: "Sebrae - Curso Educação Financeira Empresarial",
+    file: "/formations/educacao-financeira.pdf"
   },
   {
     id: 16,
     img: "/imgs/imgEducation/sebrae-GF.webp",
     text: "Sebrae - Curso Gestão Financeira",
+    file: "/formations/gestao-financeira.pdf"
   },
   {
     id: 17,
     img: "/imgs/imgEducation/sebrae-CS.webp",
     text: "Sebrae - Curso Customer Success(Como Conquistar e Manter Clientes)",
+    file: "/formations/customer-success.pdf"
   },
   {
     id: 18,
     img: "/imgs/imgEducation/sebrae-MD.webp",
     text: "Sebrae - Curso Marketing digital para empreendedor",
+    file: "/formations/marketing-digital.pdf"
   },
   {
     id: 19,
     img: "/imgs/imgEducation/sebrae-EFC.webp",
     text: "Sebrae - Curso Estratégia Financeira para o Crescimento",
+    file: "/formations/estrategia-financeira.pdf"
   },
   {
     id: 20,
     img: "/imgs/imgEducation/sebrae-PE.webp",
     text: "Sebrae - Curso Planejamento Estratégico para Empreendedores",
+    file: "/formations/planejamento-estrategico.pdf"
   },
   {
     id: 21,
     img: "/imgs/imgEducation/fgv.webp",
     text: "FGV - Curso Segurança Digital",
+    file: "/formations/seguranca-digital.pdf"
   },
   {
     id: 22,
     img: "/imgs/imgEducation/24h.webp",
     text: "Cursos 24h - Auxiliar Administrativo",
+    file: "/formations/auxiliar-administrativo.pdf"
   },
-]
+  {
+    id: 23,
+    img: "/imgs/imgEducation/testes.webp",
+    text: "Digital Innovation - Testes Automatizados com Cypress",
+    file: "/formations/testes-automatizados"
+  }
+];
 
-export default cardsServer
+export default cardsServer;
