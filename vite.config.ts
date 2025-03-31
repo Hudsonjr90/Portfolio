074d369ba86a519a05231254ae0c9d64f4cf417d
Hudson90kj <hudson.oliveira@dynaton.com.br>
2025-03-30 23:10:02 -0300

    feat: add new testimonials

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ filename: 'stats.html', open: true }) as any 
+    visualizer({ filename: "stats.html", open: true }) as any,
   ],
   css: {
     modules: {
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
     },
   },
   optimizeDeps: {
-    include: ['react', 'react-dom'],
-    exclude: ['js-big-decimal'],
+    include: ["react", "react-dom"],
+    exclude: ["js-big-decimal"],
     force: true,
-    entries: ['src/main.tsx']
+    entries: ["src/main.tsx"],
   },
   build: {
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            const moduleName = id.split('/')[2];
-            if (['react', 'react-dom', 'react-router-dom'].includes(moduleName)) {
-              return 'vendor';
+          if (id.includes("node_modules")) {
+            const moduleName = id.split("/")[2];
+            if (
+              ["react", "react-dom", "react-router-dom"].includes(moduleName)
+            ) {
+              return "vendor";
             }
-            if (moduleName.startsWith('@')) {
-              return moduleName.split('/')[0];
+            if (moduleName.startsWith("@")) {
+              return moduleName.split("/")[0];
             }
-            return 'libs';
+            return "libs";
           }
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
+
+
