cfea9ea2bda12f15b68b57de450d493145a08bf0
Hudsonjr90 <hudsonhugo@bol.com.br>
2025-10-14 22:55:43 -0300

    feat: add pagination styles and remove ChatBot component for improved UI consistency

--- src/components/Card/CardComponent.tsx
+++ src/components/Card/CardComponent.tsx
@@ -207,6 +207,7 @@ const CardComponent = () => {
         initial={{ opacity: 0, y: "80%" }}
         animate={{ opacity: 1, y: "0%" }}
         transition={{ duration: 2, delay: 0.3, ease: [0.3, 0, 0.2, 1] }}
+        className={styles.pagination_container}
       >
         <Paginate
           pageCount={pageCount}
